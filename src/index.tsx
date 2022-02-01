7acc8946c784176915569db7c415e63028f575c1
Your Name <65797650+Pyoncha@users.noreply.github.com>
2022-02-01 22:55:39 +0900

    update_history

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ import {
 
 import { Editor } from './pages/editor';
 import { History } from './pages/history';
-
+import { useStateWithStorage } from './hooks/use_state_with_storage';
 
 const GlobalStyle = createGlobalStyle`
   body * {
@@ -19,19 +19,32 @@ const GlobalStyle = createGlobalStyle`
   }
 `;
 
-const Main = (
-  <>
-    <GlobalStyle />
-    <Router>
-      <Route exact path="/editor">
-        <Editor />
-      </Route>
-      <Route exact path="/history">
-        <History />
-      </Route>
-      <Redirect to="/editor" path="*" />
-    </Router>
-  </>
-);
+const StorageKey = '/editor:text'
 
-render(Main, document.getElementById("app"));
+const Main: React.FC = () => {
+  const [text, setText] = useStateWithStorage('', StorageKey);
+  
+  return (
+    <>
+      <GlobalStyle />
+      <Router>
+        <Switch>
+          <Route exact path="/editor">
+            <Editor
+              text={text}
+              setText={setText}
+            />
+          </Route>
+          <Route exact path="/history">
+            <History
+              setText={setText}
+            />
+          </Route>
+          <Redirect to="/editor" path="*" />
+        </Switch>
+      </Router>
+    </>
+  );
+};
+
+render(<Main />, document.getElementById("app"));
